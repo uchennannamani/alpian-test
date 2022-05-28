d1f725a713e027be77af554b3bf2614f9849f279
uchennannamani <nnamaniuche@outlook.com>
2022-05-29 00:17:56 +0100

    set up springboot service, maven dependencies and properties files

---
+++ Uche Nnamani/exercise-3/src/test/java/com/uche/customermapperservice/CustomerMapperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uche.customermapperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMapperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
