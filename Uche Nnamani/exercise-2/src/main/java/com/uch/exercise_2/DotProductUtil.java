cc04c5a9360c66fab1532b8e52e006757d607097
uchennannamani <nnamaniuche@outlook.com>
2022-05-28 22:19:45 +0100

    completed the for-loop implementation. Implement map-reduce option

--- Uche Nnamani/exercise-2/src/main/java/com/uch/exercise_2/DotProductUtil.java
+++ Uche Nnamani/exercise-2/src/main/java/com/uch/exercise_2/DotProductUtil.java
@@ -5,14 +5,17 @@ public class DotProductUtil {
     public int calculateDotProductUsingForLoop(int[] a, int[] b) {
         int sum = 0;
 
+        if (a.length != b.length) {
+
+            throw new IllegalArgumentException("The lengths of both arrays must be equal");
+        } else {
 
             for (int i = 0; i < a.length; i++) {
                 int tempProduct = a[i] * b[i];
                 sum = sum + tempProduct;
             }
-
+        }
         System.out.println("The dotProduct = " + sum);      // remove at the end
         return sum;
     }
-
 }
--- Uche Nnamani/exercise-2/src/test/java/com/uch/exercise_2/DotProductUtilTest.java
+++ Uche Nnamani/exercise-2/src/test/java/com/uch/exercise_2/DotProductUtilTest.java
@@ -1,19 +1,36 @@
 package com.uch.exercise_2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DotProductUtilTest {
+    DotProductUtil dotProductUtil;
+
+    @BeforeEach
+    void setUp() {
+
+        dotProductUtil = new DotProductUtil();
+    }
+
+    int[] a = {1, 2, 3, 4};
+    int[] b = {5, 6, 7, 8};
+    int[] c = {1, 2, 3, 4, 5, 6};
+    int[] d = {7, 8, 9, 10, 11, 12};
 
     @Test
     void calculateDotProductUsingForLoopTest_1() {
-       DotProductUtil dotProductUtil = new DotProductUtil();
-        int[] a = {1, 2, 3, 4};
-        int[] b = {5, 6, 7, 8};
 
         assertEquals(70, dotProductUtil.calculateDotProductUsingForLoop(a, b));
-
+        assertEquals(217, dotProductUtil.calculateDotProductUsingForLoop(c, d));
     }
 
+    @Test
+    void calculateDotProductUsingForLoopTest_2() {
+
+        assertThrows(IllegalArgumentException.class, () -> dotProductUtil.calculateDotProductUsingForLoop(a, c));
+        assertThrows(IllegalArgumentException.class, () -> dotProductUtil.calculateDotProductUsingForLoop(a, d));
+    }
 }
